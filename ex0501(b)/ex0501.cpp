ee67cc84b6f8c186c545ee72023f310dcb15a9c1
mrban <3447743076@qq.com>
2025-12-18 00:25:31 +0800

    Add files via upload

---
+++ ex0501(b)/ex0501.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+int main()
+{
+	int number;
+	printf("Enter a number:");
+	scanf_s("%d", &number);
+	
+	if (number % 2 == 0) {
+		printf("NUMBER IS EVEN\n");
+	}
+	else {
+		printf("NUMBER IS ODD\n");
+	}
+	return 0;
+}
