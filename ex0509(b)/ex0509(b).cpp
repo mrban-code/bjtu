4cf90eac7477d5cd65d2dee7e139b86c7e557982
mrban <3447743076@qq.com>
2025-12-18 10:51:28 +0800

    Add files via upload

---
+++ ex0509(b)/ex0509(b).cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+int main()
+{
+	float x;
+	printf("请输入x的值：");
+	scanf_s("%f", &x);
+	 
+	if (x < 0) {
+		printf("y的值为：1\n");
+	}
+	else if (x == 0) {
+		printf("y的值为：0\n");
+	}
+	else {
+		printf("y的值为：-1\n");
+	}
+	return 0;
+}
